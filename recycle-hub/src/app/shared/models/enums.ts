c574a639cefb46194ca49b8a3200a377c4b4c6a2
ayounsni <Ayoubsnini@gmail.com>
2025-02-04 11:43:34 +0100

    Add enums for WasteType, CollectRequestStatus, and UserRole

---
+++ recycle-hub/src/app/shared/models/enums.ts
@@ -0,0 +1,5 @@
+export type WasteType = 'plastic' | 'glass' | 'paper' | 'metal';
+
+export type CollectRequestStatus = 'pending' | 'occupied' | 'in-progress' | 'validated' | 'rejected';
+
+export type UserRole = 'user' | 'collector';
